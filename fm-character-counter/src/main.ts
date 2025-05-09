b9a21ed23610cac5ff19f0ba72c62023bb25ead5
Sandrine BRIVAL <brival.sandrine@gmail.com>
2025-05-09 13:41:10 +0200

    feat(layout): add counters and letter density sections

--- fm-character-counter/src/main.ts
+++ fm-character-counter/src/main.ts
@@ -18,9 +18,26 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
              </fieldset>
              <p>Approx. reading time: 0 minute</p>
         </div>
-
     </form>
   </section>
+  <section>
+    <div id="counters">
+        <p id="counter-character">
+           <span>00</span>
+            <span>Total Characters</span>
+        </p>
+        <p id="counter-word">
+            <span>00</span>
+            <span>Word Count</span>
+        </p>
+        <p id="counter-sentence">
+            <span>00</span>
+            <span>Sentence Count</span>
+        </p>
+    </div>
+  </section>
+  <section id="letter-density">
+    <h2>Letter Density</h2>
+    <p>No characters found. Start typing to see letter density.</p>
+  </section>
 `
-
-//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
